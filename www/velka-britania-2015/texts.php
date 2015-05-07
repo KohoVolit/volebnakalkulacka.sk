6ae3573119e902353d22bc527745e371f89e6f6a
Michal Skop <michal.skop@kohovolit.eu>
2015-05-07 03:05:53 +0200

    GB 2015

---
+++ www/velka-britania-2015/texts.php
@@ -0,0 +1,101 @@
+<?php
+/**
+* Texts (for translation)
+*/
+$text = array(
+  
+  'election_code' => 'gb-2015',
+  'template_code' => 'gb-2015',
+  'lang' => 'sk',
+  //meta
+  'author' => 'KohoVolit.eu',
+  'description' => 'Voľby ve Velkej Británii 2015',
+  'keywords' => 'voľby 2015, volebný test',
+  //fb
+  'og:image' => 'http://volebnakalkulacka.sk/image/logo.jpg',
+  'og:title' => 'Volebná kalkulačka',
+  'og:url' => 'http://volebnakalkulacka.sk',
+  'og:site_name' => 'Volebná kalkulačka',
+  'author_link' => 'http://kohovolit.eu',
+  'licence' => 'Creative Commons BY 4.0',
+  'year' => '2015',
+  
+  'footer_text' => 'Volebné kalkulačky pre Vás už od roku 2006 vytvára občianske združenie <a href="http://kohovolit.eu">KohoVolit.eu</a>',
+  
+
+  'calc' => 'Volebná kalkulačka',
+  'info' => 'Info',
+  'url' => 'http://volebnakalkulacka.sk',  
+  'title' => 'Volebná kalkulačka 2015 | Veľká Británia' ,
+
+   //PAGE
+  'calc_name' => 'Volebná kalkulačka - Veľká Británia 2015',
+  'page_question_yes' => 'ÁNO',
+  'page_question_no' => 'NIE',
+  'page_question_neutral' => 'neviem',
+  'page_question_neutral_add' => ' / je mi to jedno',
+  'page_important_questions' => 'Dôležité otázky', 
+  'page_important_legend' => '<strong>Označte otázky, ktoré sú pre Vás najdôležitejšie</strong>. Spresníte tým výsledok. Tento krok nie je povinný a môžete ho preskočiť.',
+  'page_important_skip' => 'Preskočiť',
+  'page_important_results' => 'Výsledky', 
+  
+  'ie8' => 'Omlouváme se, ale Volební kalkulačka nemusí fungovat v prohlížečích Internet Explorer 8 a starších',
+  'opera11' =>' Omlouváme se, ale Volební kalkulačka nemusí fungovat v prohlížečích Opera 11 a starších',
+
+   //MATCH
+   'match_save' => 'Uložiť svoje hlasovanie',
+ 
+   'result_winner_share' => 'Zdieľaj výsledky na Facebooku',
+   'result_share' => 'Zdieľaj výsledky',
+   
+   //fb
+   'result_fb_appid' => "647475721940594",
+  'result_fb_picture_link' => 'http://volebnakalkulacka.sk/velka-britania-2015/image/logo/',
+  'result_fb_name' => 'V "Britskej" Volebnej kalkulačke mám najvyššiu zhodu:',
+  'result_fb_description_1' => 'Podľa Volebnej kalkulačky pre voľby vo V.Británii sa so mnou najviac zhodujú: ',
+  'result_fb_description_match' => 'zhoda',
+  'result_fb_description_and' => 'a',
+  'result_fb_description_only' => 'len',
+  'result_fb_description_2' => 'Najhorší je ',
+  'result_fb_description_3' => 'S kým sa najviac zhodujete na VolebnaKalkulacka.sk?',
+  'result_fb_redirect_link' => 'http://volebnakalkulacka.sk/',
+  'result_fb_link' => 'http://volebnakalkulacka.sk/',
+  'result_fb_action' => "[{'name':'Koho voliť?','link':'http://volebnakalkulacka.sk/'}]",   
+   
+   //tabs
+   'result_results' => 'Výsledky',
+   //'result_comparison' => 'Porovnání',
+   //'result_eu' => 'Výsledky v EU',
+   
+   //support (DMS)
+   'result_support' => '<strong>Len z ČR: Podpořte tvorbu nezávislých
+  Volebních kalkulaček.</strong><br/>
+  Pošlete SMS na&nbsp;číslo&nbsp;<strong><span class="colorstrong">87777</span></strong>
+  s&nbsp;textem <strong><span class="colorstrong">DMS&nbsp;KOHOVOLIT</span></strong>
+  (cena 30Kč, z toho 28Kč pro
+   sdružení KohoVolit.eu) nebo 
+   <strong><span class="colorstrong">DMS&nbsp;ROK&nbsp;KOHOVOLIT</span></strong>
+   (automaticky každý měsíc po 1 rok)',
+   
+   //comparison
+   'result_close' => 'Zavrieť',
+   'result_yes' => 'Áno',
+   'result_no' => 'Nie',
+   'result_question' => 'Otázka',
+   
+   //save
+   'result_save_description' => 'K výsledkom svojho hlasovania sa môžete kedykoľvek vrátiť, ak si skopírujete a uložíte celú túto adresu:',
+   
+   //google analytics tracking id
+   'google_tracking_id' => 'UA-8592359-12',    
+
+
+  'result_twitter_data_lang' => 'sk',
+  'result_twitter_text_1' => '"Britská" Vol.kalkulačka:1.',
+  'result_twitter_text_3' => ', naopak ',
+  'result_twitter_account' => 'KohoVolitEU',
+  'result_twitter_hashtags' => 'voľby,voľby2015',
+  'result_twitter_share' => 'Tweetni',
+  
+  
+);
