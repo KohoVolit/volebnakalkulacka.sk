219d36b8f3572aaacead0c8c6b7909ff29b55991
Michal Skop <michal.skop@kohovolit.eu>
2019-05-15 16:15:37 +0200

    update

--- www/index.php
+++ www/index.php
@@ -78,7 +78,7 @@
                 <strong>Volebná kalkulačka Európsky parlament 2019</strong> ukazuje vašu zhodu s kandidátmi v európskych voľbách.
 
                 <div style="padding-bottom:3em">
-                  <a href="sk/europsky-parlament-2019?hr=<?php if( isset($_SERVER['HTTP_REFERER'])) { $hr=$_SERVER['HTTP_REFERER']; if ($hr!="") {echo base64_encode($hr);} } ?><?php if( isset($_GET['ref'])) { $ref=$_GET['ref']; if ($ref!="") {echo '&ref=' . $ref;} } ?>" class="btn btn btn-success btn-lg" disabled="disabled">SPUSTIŤ VOLEBNÚ KALKULAČKU</a>
+                  <a href="sk/europsky-parlament-2019?hr=<?php if( isset($_SERVER['HTTP_REFERER'])) { $hr=$_SERVER['HTTP_REFERER']; if ($hr!="") {echo base64_encode($hr);} } ?><?php if( isset($_GET['ref'])) { $ref=$_GET['ref']; if ($ref!="") {echo '&ref=' . $ref;} } ?>" class="btn btn btn-success btn-lg">SPUSTIŤ VOLEBNÚ KALKULAČKU</a>
                 </div>
                 </p>
                <p class="lead">
