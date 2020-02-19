cb35f35172fbc219de58463bce8fa22fb13e1fa3
Michal Skop <michal.skop@kohovolit.eu>
2020-02-20 00:32:48 +0100

    initial vue 2020

---
+++ vue/volby-2020/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="text-muted mt-4 md-4">
+        <div class="container">
+            <div class="row justify-center">
+                <div class="col-md-3">
+
+                </div>
+                <div class="col-md-6 text-center">
+                    <a href="http://kohovolit.eu" title="KohoVolit.eu" id="footer-kohovolit-logo" target="_blank"><img src="https://volebnakalkulacka.azureedge.net/image/logo.png" alt="logo kohovolit.eu"></a>
+					<p class="lead black">Volebné kalkulačky pre Vás už od roku 2006 vytvára občianske združenie <a href="http://kohovolit.eu" target="_blank">KohoVolit.eu</a></p>
+                    <hr>
+					<p class="black">Creative Commons BY NC 4.0 | <a href="http://kohovolit.eu" id="footer-kohovolit-licence">KohoVolit.eu</a> 2020</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+
+</script>
